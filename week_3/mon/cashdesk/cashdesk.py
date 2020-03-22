69f87fbb9c4934baa662137a7273f2c4b0249506
Boyko03 <boyko100@abv.bg>
2020-03-23 01:10:28 +0200

    Added cashdesk task

--- week_3/mon/cashdesk/cashdesk.py
+++ week_3/mon/cashdesk/cashdesk.py
@@ -13,10 +13,59 @@ class Bill:
 		return f'A {self.amount}$ bill'
 
 	def __repr__(self):
-		return f'{self.amount}'
+		return f'A {self.amount}$ bill'
 
 	def __eq__(self, other):
 		return True if self.amount == other.amount else False
+
+	def __hash__(self):
+		return hash(self.amount)
+
+class BillBatch:
+	def __init__(self, bills):
+		self.bills = bills
+
+	def __len__(self):
+		return len(self.bills)
+
+	def total(self):
+		total = 0
+		for bill in self.bills:
+			total += int(bill)
+
+		return total
+
+	def __getitem__(self, index):
+		return self.bills[index]
+
+class CashDesk:
+	def __init__(self):
+		self.money_holder = {}
+
+	def take_money(self, money):
+		if type(money) == Bill:
+			if money in self.money_holder:
+				self.money_holder[money] += 1
+			else:
+				self.money_holder[money] = 1
+			return
+
+		for bill in money:
+			if bill in self.money_holder:
+				self.money_holder[bill] += 1
+			else:
+				self.money_holder[bill] = 1
+
+	def total(self):
+		t = 0
+		for bill in self.money_holder:
+			t += int(bill) * self.money_holder[bill]
+
+		return t
+
+	def inspect(self):
+		for bill in self.money_holder:
+			print(f'{int(bill)}$ bills - {self.money_holder[bill]}')
 
 if __name__ == '__main__':
 	main()
--- week_3/mon/cashdesk/test_cashdesk.py
+++ week_3/mon/cashdesk/test_cashdesk.py
@@ -1,5 +1,5 @@
 import unittest
-from cashdesk import Bill
+from cashdesk import Bill, BillBatch, CashDesk
 
 class TestBill(unittest.TestCase):
 	def test_initialization(self):
@@ -68,6 +68,103 @@ class TestBill(unittest.TestCase):
 
 		self.assertTrue(a != b)
 		self.assertTrue(a == c)
+
+	def test_hash_function(self):
+		money_holder = {}
+		a = Bill(10)
+		b = Bill(10)
+
+		money_holder[a] = 1
+		if b in money_holder:
+			money_holder[b] += 1
+
+		expexted = 2
+		self.assertEqual(expexted, money_holder[a])
+
+	def test_int_function(self):
+		a = Bill(10)
+
+		result = int(a)
+
+		expexted = 10
+		self.assertEqual(result, expexted)
+
+class TestBatchBill(unittest.TestCase):
+	def test_get_len_of_bill_batch(self):
+		values = [10, 20, 50, 100]
+		bills = [Bill(value) for value in values]
+
+		batch = BillBatch(bills)
+
+		self.assertEqual(len(batch), 4)
+
+	def test_total(self):
+		values = [10, 20, 50, 100]
+		bills = [Bill(value) for value in values]
+
+		batch = BillBatch(bills)
+		total = batch.total()
+
+		expexted = 180
+		self.assertEqual(total, expexted)
+
+	def test_iteration_of_bill_batch(self):
+		values = [10, 20, 50, 100]
+		bills = [Bill(value) for value in values]
+		batch = BillBatch(bills)
+
+		result = ""
+		for bill in batch:
+			result += f'{bill}\n'
+
+		expexted = "A 10$ bill\nA 20$ bill\nA 50$ bill\nA 100$ bill\n"
+		self.assertEqual(result, expexted)
+
+class TestCashDesk(unittest.TestCase):
+	def test_take_money_batch(self):
+		values = [10, 20, 50, 100, 100, 100]
+		bills = [Bill(value) for value in values]
+
+		batch = BillBatch(bills)
+
+		desk = CashDesk()
+
+		desk.take_money(batch)
+
+	def test_total_if_cash_desk_is_empty(self):
+		values = [10, 20, 50, 100, 100, 100]
+		bills = [Bill(value) for value in values]
+		batch = BillBatch(bills)
+		desk = CashDesk()
+
+		result = desk.total()
+
+		expexted = 0
+		self.assertEqual(result, expexted)
+
+	def test_total_when_cash_desk_is_not_empty(self):
+		values = [10, 20, 50, 100, 100, 100]
+		bills = [Bill(value) for value in values]
+		batch = BillBatch(bills)
+		desk = CashDesk()
+
+		desk.take_money(batch)
+		desk.take_money(Bill(10))
+		result = desk.total()
+
+		expexted = 390
+		self.assertEqual(result, expexted)
+
+	def test_inspect_function(self):
+		values = [10, 20, 50, 100, 100, 100]
+		bills = [Bill(value) for value in values]
+		batch = BillBatch(bills)
+		desk = CashDesk()
+
+		desk.take_money(batch)
+		desk.take_money(Bill(10))
+		
+		desk.inspect()
 
 if __name__ == '__main__':
 	unittest.main()
