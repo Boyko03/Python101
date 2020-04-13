b45ad25fe0e86080c4b02c9d8bdf68c8cd160cba
Boyko03 <boyko100@abv.bg>
2020-04-13 18:26:42 +0300

    Completed book reader generator task.

--- week_6/generators/book_reader.py
+++ week_6/generators/book_reader.py
@@ -1,14 +1,13 @@
-import subprocess
-
-
 def book_reader(book):
-    width, heigth = subprocess.check_output(['stty', 'size']).decode().split()
     for file in book:
         with open(file, 'r') as f:
-            a = ' '
-            while a != '':
+            a = f.readline()
+            new = a
+            while new != '':
                 lines = []
-                lines += f.readlines(int(heigth) - 1)
-                a = f.readline()
-                lines.append(a)
+                lines.append(new)
+                new = f.readline()
+                while new != '' and new[0] != '#':
+                    lines.append(new)
+                    new = f.readline()
                 yield ''.join(lines)
--- week_6/generators/test_book_reader.py
+++ week_6/generators/test_book_reader.py
@@ -1,5 +1,5 @@
 import unittest
-import sys
+import curses
 from book_reader import book_reader
 
 
@@ -8,14 +8,21 @@ class TestBookReaderGenerator(unittest.TestCase):
         book = book_reader(('001.txt', '002.txt'))
 
         c = ''
-        while(c != 'q'):
-            c = sys.stdin.read(1)
-            if c == ' ':
-                try:
-                    print(next(book))
-                except StopIteration:
-                    print('End of book')
-                    break
+        try:
+            win = curses.initscr()
+            while(c != 'q'):
+                c = chr(win.getch())
+                win.clear()
+                if c == ' ':
+                    try:
+                        win.addstr(0, 0, next(book))
+
+                    except curses.error:
+                        pass
+                    except StopIteration:
+                        break
+        finally:
+            curses.endwin()
 
 
 if __name__ == '__main__':
