17e78859473f1acd66a10551e591b270dca70e04
Boyko03 <boyko100@abv.bg>
2020-04-28 18:57:02 +0300

    Added Task 2 first queries.

---
+++ week_9/task2/first_queries.sql
@@ -0,0 +1,37 @@
+SELECT address
+  FROM STUDIO
+  WHERE name = "MGM"
+  
+------------------------------------------------------------
+
+SELECT birthdate
+  FROM MOVIESTAR
+  WHERE name = "Kim Basinger"
+  
+------------------------------------------------------------
+
+SELECT name
+  FROM MOVIEEXEC
+  WHERE networth > 10000000
+  
+------------------------------------------------------------
+
+SELECT * FROM MOVIESTAR
+  WHERE gender = "M" OR address = "Prefect Rd."
+  
+------------------------------------------------------------
+
+INSERT INTO MOVIESTAR
+  VALUES ("Zahari Baharov", "some address", "M", 1980-08-12)
+  
+------------------------------------------------------------
+
+DELETE FROM STUDIO
+  WHERE address LIKE ("%5%")
+  
+------------------------------------------------------------
+
+UPDATE MOVIE
+  SET studioname = "Fox"
+  WHERE title LIKE ("%star%")
+
